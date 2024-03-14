3411738011a1af996cc46347faa80e8556f494bf
Sri Sandeep <sandyorton7@gmail.com>
2024-03-14 23:01:36 +0530

    Initial Ubuntu Script

---
+++ Ubuntu.sh
@@ -0,0 +1,21 @@
+#Let's make sure we're updated to the latest version
+cd ~
+sudo apt update && apt upgrade -y
+
+# Installing nala cuz it just looks so much better
+sudo apt install nala 
+sudo nala install tldr neofetch lolcat cowsays gh git vim bashtop curl bashtop
+tldr -u
+
+# Setting up git 
+git config --global user.name "Sri Sandeep"
+git config --global user.email "sandyorton7@gmail.com"
+
+# Install oh my bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+mkdir practice
+cd practice
+
+#end of script
+cowsays $USER ",Your system is now ready to rock!!" | lolcat -at

7489cf30fb9969b2d8582c14ec15fa8ddd529953
Sri Sandeep <sandyorton7@gmail.com>
2024-03-15 20:33:41 +0530

    dos2unix

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,13 +1,12 @@
-#Let's make sure we're updated to the latest version
-cd ~
+#Lets make sure were updated to the latest version
 sudo apt update && apt upgrade -y
 
 # Installing nala cuz it just looks so much better
 sudo apt install nala -y
-sudo nala install tldr neofetch lolcat cowsays gh git vim bashtop curl bashtop -y
+sudo nala install tldr neofetch lolcat cowsay gh git vim bashtop curl bashtop -y
 tldr -u
 
-# Setting up git 
+# Setting up git
 git config --global user.name "Sri Sandeep"
 git config --global user.email "sandyorton7@gmail.com"
 
@@ -18,4 +17,5 @@ mkdir practice
 cd practice
 
 #end of script
+neofetch | lolcat -at
 cowsay $USER ",Your system is now ready to rock!!" | lolcat -at

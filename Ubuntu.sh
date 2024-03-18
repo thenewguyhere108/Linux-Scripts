f93010e919e16994fe38aefcbf5f141c5df6402d
Sri Sandeep <sandyorton7@gmail.com>
2024-03-18 18:41:14 +0530

    Update Install script

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -3,18 +3,17 @@ sudo apt update && apt upgrade -y
 
 # Installing nala cuz it just looks so much better
 sudo apt install nala -y
-sudo nala install tldr neofetch lolcat cowsay gh git vim bashtop curl bashtop -y
+sudo nala install tldr neofetch lolcat cowsay gh git vim bashtop curl bashtop zsh -y
 tldr -u
+
+sudo chsh -s /usr/bin/zsh $USER
 
 # Setting up git
 git config --global user.name "Sri Sandeep"
 git config --global user.email "sandyorton7@gmail.com"
-
-# Install oh my bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+gh auth login
 
 mkdir practice
-cd practice
 
 #end of script
 neofetch | lolcat -at
